1dfaef79eec67e4e8ec49bb3d51dc6c9ef00f3d2
AbiBlast <abiblasting@gmail.com>
2026-05-09 16:14:00 -0300

    Agrega clases
    Agrega las clases "ClaseAccesoBD" y "ClaseSucursales" que gestionan el acceso a la base de datos y las consultas a la misma respectivamente

---
+++ TP5_PROGRA3/Clases/ClaseAccesoBD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TP5_PROGRA3.Clases
+{
+    public class ClaseAccesoBD
+    {
+        private const string rutaConexion = @"Data Source= (localdb)\MSSQLLocalDB;Initial Catalog=BDSucursales;Integrated Security=True;TrustServerCertificate=True";
+        //private static string rutaConexion = @"Data Source=localhost\sqlexpress;Initial Catalog=BDSucursales;Integrated Security=True";
+        //Borrar la primera y establecer la ruta comentada antes de entregar (nos vamos a olvidar)
+        public DataTable ObtenerTablaBD(string consultaSQL, string nombreTabla)
+        {
+            SqlConnection conexion = new SqlConnection(rutaConexion);
+            conexion.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(consultaSQL, conexion);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "nombreTabla");
+            conexion.Close();
+            return ds.Tables["nombreTabla"];
+        }
+
+        public int ConsultaBD(string consultaSQL)
+        {
+            SqlConnection conexion = new SqlConnection(rutaConexion);
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand(consultaSQL, conexion);
+            int filas = (int)cmd.ExecuteNonQuery(); // PARA INSERT-UPDATE-DELETE
+            conexion.Close();
+            return filas;
+        }
+    }
+}
--- TP5_PROGRA3/Ejercicios/AgregarSucursal.aspx.cs
+++ TP5_PROGRA3/Ejercicios/AgregarSucursal.aspx.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TP5_PROGRA3.Clases;
 
 namespace TP5_PROGRA3.Ejercicios
 {
     public partial class AgregarSucursal : System.Web.UI.Page
     {
+        ClaseSucursales Test = new ClaseSucursales();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                ddlTest1.DataSource = Test.ObtenerHorarios();
+                ddlTest1.DataTextField = "DescripcionHorario";
+                ddlTest1.DataValueField = "Id_Horario";
+                ddlTest1.DataBind();
+
+                ddlTest2.DataSource = Test.ObtenerProvincias();
+                ddlTest2.DataTextField = "DescripcionProvincia";
+                ddlTest2.DataValueField = "Id_Provincia";
+                ddlTest2.DataBind();
+
+            }
 
         }
     }
